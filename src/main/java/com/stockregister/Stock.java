0a5b6601fba84822aea2cd1fd7045b6191329838
Satyam-Maru <satyammaru07@gmail.com>
2024-08-18 23:38:48 +0530

    StockInOut functionality added.

--- src/main/java/com/stockregister/Stock.java
+++ src/main/java/com/stockregister/Stock.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.print.PrinterException;
-
 
 public class Stock extends JFrame implements ActionListener {
 
@@ -16,12 +14,15 @@ public class Stock extends JFrame implements ActionListener {
 
     // Swing components
     // ------------------------------------------------------------------------------------------
-    protected JPanel stockPanel, buttonsPanel, addItemPanel, panelStack;
-    protected JButton addBtn, stockInBtn, stockOutBtn, removeBtn, updateBtn, prevBtn, doneBtn;
-    protected JLabel itemLabel, categoryLabel, purchaseLabel, sellingLabel,
-                      priceUnitLabel, openingStockLabel, partyLabel;
-    protected JTextField itemTF, categoryTF, purchaseTF, sellingTF, partyTF, openingStockTF;
-    private JComboBox<String> priceUnit;
+    protected JPanel stockPanel, buttonsPanel, addItemPanel, panelStack, stockInOutPanel;
+    protected JButton addBtn, stockInOutBtn, removeBtn, updateBtn, prevBtn, addItemDoneBtn,
+                        stockInBtn, stockOutBtn;
+    protected JLabel itemLabel, categoryLabel, purchaseLabel, sellingLabel, // addItemPanel
+                      priceUnitLabel, openingStockLabel, partyLabel;        // addItemPanel
+    protected JLabel stPartyLabel, stCategoryLabel, stItemLabel, stQuantityLabel, stPriceLabel;
+    protected JTextField itemTF, categoryTF, purchaseTF, sellingTF, partyTF, openingStockTF; // addItemPanel
+    protected JTextField stPartyTF, stQuantityTF, stPriceTF;
+    private JComboBox<String> priceUnit, stCategoryComboBox, stItemComboBox;
     // ------------------------------------------------------------------------------------------
 
     // Variables
@@ -30,6 +31,13 @@ public class Stock extends JFrame implements ActionListener {
     protected double purchase_price, selling_price, MRP;
     // ------------------------------------------------------------------------------------------
 
+    // getters
+    protected JPanel getPanel(){
+        return stockPanel;
+    }
+
+    // setters
+    // ------------------------------------------------------------------------------
     protected void setPanel(){
         stockPanel = new JPanel();
         stockPanel.setBounds(140, 60, 1035, 640);
@@ -55,27 +63,184 @@ public class Stock extends JFrame implements ActionListener {
         addBtn.setForeground(Color.BLACK);
         prevBtn = addBtn;
 
-        stockInBtn = initButton(stockInBtn, "Stock In");
-        stockInBtn.setBounds(55, 140, 135, 40);
-
-        stockOutBtn = initButton(stockOutBtn, "Stock Out");
-        stockOutBtn.setBounds(40, 230, 160, 40); // x occupied = 390
+        stockInOutBtn = initButton(stockInOutBtn, "Stock In/Out");
+        stockInOutBtn.setBounds(5, 140, 250, 40);
 
         removeBtn = initButton(removeBtn, "Remove an Item");
-        removeBtn.setBounds(5, 320, 250, 40);
+        removeBtn.setBounds(5, 230, 250, 40);
 
         updateBtn = initButton(updateBtn, "Update an Item");
-        updateBtn.setBounds(5, 410, 250, 40); // y occupied = 300
+        updateBtn.setBounds(5, 320, 250, 40); // y occupied = 300
 
         buttonsPanel.add(addBtn);
-        buttonsPanel.add(stockInBtn);
-        buttonsPanel.add(stockOutBtn);
+        buttonsPanel.add(stockInOutBtn);
         buttonsPanel.add(removeBtn);
         buttonsPanel.add(updateBtn);
     }
+    //  ------------------------------------------------------------------------------
 
-    protected JPanel getPanel(){
-        return stockPanel;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == addBtn){
+            System.out.println("add btn");
+            highlightBtn(addBtn);
+            showPanelStack("Add Item Panel");
+        }
+        else if (e.getSource() == stockInOutBtn){
+            highlightBtn(stockInOutBtn);
+            System.out.println("stockIn btn");
+            showPanelStack("Stock In Out Panel");
+        }
+        else if (e.getSource() == removeBtn){
+            highlightBtn(removeBtn);
+            System.out.println("remove btn");
+        }
+        else if (e.getSource() == updateBtn){
+            highlightBtn(updateBtn);
+            System.out.println("Update btn");
+        }
+    }
+
+    // adders / setters
+    // ------------------------------------------------------------------------------
+    private void setAddItemLabels() {
+
+        itemLabel = initLabel(itemLabel, "Item Name");
+        itemLabel.setBounds(50, 55, 170, 30);
+        addItemPanel.add(itemLabel);
+
+        categoryLabel = initLabel(categoryLabel, "Category");
+        categoryLabel.setBounds(itemLabel.getWidth() + 250, itemLabel.getY(), 150, 30);
+        addItemPanel.add(categoryLabel);
+
+        purchaseLabel = initLabel(purchaseLabel, "Purchase Price");
+        purchaseLabel.setBounds(itemLabel.getX(), itemLabel.getY() + 100, 200, 30);
+        addItemPanel.add(purchaseLabel);
+
+        sellingLabel = initLabel(sellingLabel, "Selling Price");
+        sellingLabel.setBounds(itemLabel.getWidth() + 250, purchaseLabel.getY(), 200, 30);
+        addItemPanel.add(sellingLabel);
+
+        partyLabel = initLabel(partyLabel, "Party");
+        partyLabel.setBounds(itemLabel.getX(), purchaseLabel.getY() + 100, 80, 30);
+        addItemPanel.add(partyLabel);
+
+        priceUnitLabel = initLabel(priceUnitLabel, "Price Unit");
+        priceUnitLabel.setBounds(itemLabel.getWidth() + 250, partyLabel.getY(), 200, 30);
+        addItemPanel.add(priceUnitLabel);
+
+        openingStockLabel = initLabel(openingStockLabel, "Opening Stock");
+        openingStockLabel.setBounds(itemLabel.getX(), partyLabel.getY() + 100, 200, 30);
+        addItemPanel.add(openingStockLabel);
+    }
+
+    private void setAddItemTextField() {
+
+        itemTF = initTextField(itemTF);
+        itemTF.setBounds(itemLabel.getX(), itemLabel.getY() + itemLabel.getHeight() + 7, 200, 30);
+        addItemPanel.add(itemTF);
+
+        categoryTF = initTextField(categoryTF);
+        categoryTF.setBounds(categoryLabel.getX(), categoryLabel.getY() + categoryLabel.getHeight() + 7, 140, 30);
+        addItemPanel.add(categoryTF);
+
+        purchaseTF = initTextField(purchaseTF);
+        purchaseTF.setBounds(purchaseLabel.getX(), purchaseLabel.getY() + purchaseLabel.getHeight() + 7, 120, 30);
+        addItemPanel.add(purchaseTF);
+
+        sellingTF = initTextField(sellingTF);
+        sellingTF.setBounds(sellingLabel.getX(), sellingLabel.getY() + sellingLabel.getHeight() + 7, 120, 30);
+        addItemPanel.add(sellingTF);
+
+        partyTF = initTextField(partyTF);
+        partyTF.setBounds(partyLabel.getX(), partyLabel.getY() + partyLabel.getHeight() + 7, 150, 30);
+        addItemPanel.add(partyTF);;
+
+        // for price unit (combo box)
+        String[] elements = {"Hello", "Hi"};
+        priceUnit = new JComboBox<>(elements);
+        priceUnit.setBounds(priceUnitLabel.getX(), priceUnitLabel.getY() + priceUnitLabel.getHeight() + 7, 120, 30);
+        addItemPanel.add(priceUnit);
+
+        openingStockTF = initTextField(openingStockTF);
+        openingStockTF.setBounds(openingStockLabel.getX(), openingStockLabel.getY() + openingStockLabel.getHeight() + 7, 120, 30);
+        addItemPanel.add(openingStockTF);
+
+        addItemDoneBtn = initButton(addItemDoneBtn, "Done");
+        addItemDoneBtn.setBounds(categoryLabel.getX() - 150,openingStockTF.getY() + openingStockTF.getHeight() + 100, 150, 50);
+        addItemDoneBtn.setForeground(Color.WHITE);
+        addItemDoneBtn.setBackground(Color.BLACK);
+        addItemPanel.add(addItemDoneBtn);
+    }
+
+    private void setStockInOutLabels(){
+        stPartyLabel = initLabel(stPartyLabel, "Party");
+        stPartyLabel.setBounds(80, 80, 170, 30);
+        stockInOutPanel.add(stPartyLabel);
+
+        stCategoryLabel = initLabel(stCategoryLabel, "Category");
+        stCategoryLabel.setBounds(stPartyLabel.getWidth() + 250, stPartyLabel.getY(), 150, 30);
+        stockInOutPanel.add(stCategoryLabel);
+
+        stItemLabel = initLabel(stItemLabel, "Item");
+        stItemLabel.setBounds(stPartyLabel.getX(), stPartyLabel.getY() + 100, 200, 30);
+        stockInOutPanel.add(stItemLabel);
+
+        stQuantityLabel = initLabel(stQuantityLabel, "Quantity");
+        stQuantityLabel.setBounds(stPartyLabel.getX(), stItemLabel.getY() + 100, 200, 30);
+        stockInOutPanel.add(stQuantityLabel);
+
+        stPriceLabel = initLabel(stPriceLabel, "Price");
+        stPriceLabel.setBounds(stCategoryLabel.getX(), stQuantityLabel.getY(), 80 , 30);
+        stockInOutPanel.add(stPriceLabel);
+    }
+
+    private void setStockInOutTextF_ComboBox(){
+        stPartyTF = initTextField(stPartyTF);
+        stPartyTF.setBounds(stPartyLabel.getX(), stPartyLabel.getY() + stPartyLabel.getHeight() + 7, 80, 30);
+        stockInOutPanel.add(stPartyTF);
+
+        String elements[] = {"nice", "ohk"};
+        stCategoryComboBox = new JComboBox<>(elements);
+        stCategoryComboBox.setBounds(stCategoryLabel.getX(), stCategoryLabel.getY() + stCategoryLabel.getHeight() + 7, 150, 30);
+        stockInOutPanel.add(stCategoryComboBox);
+
+        stItemComboBox = new JComboBox<>(elements);
+        stItemComboBox.setBounds(stItemLabel.getX(), stItemLabel.getY() + stItemLabel.getHeight() + 7, 150, 30);
+        stockInOutPanel.add(stItemComboBox);
+
+        stQuantityTF = initTextField(stQuantityTF);
+        stQuantityTF.setBounds(stQuantityLabel.getX(), stQuantityLabel.getY() + stQuantityLabel.getHeight() + 7, 80, 30);
+        stockInOutPanel.add(stQuantityTF);
+
+        stPriceTF = initTextField(stPriceTF);
+        stPriceTF.setBounds(stPriceLabel.getX(), stPriceLabel.getY() + stPriceLabel.getHeight() + 7, 80, 30);
+        stockInOutPanel.add(stPriceTF);
+
+        stockInBtn = initButton(stockInBtn, "Stock In");
+        stockInBtn.setBounds(stQuantityLabel.getX() + stQuantityLabel.getWidth() - 150,
+                             stQuantityTF.getY() + stQuantityTF.getHeight() + 200 ,150, 50);
+        stockInBtn.setForeground(Color.WHITE);
+        stockInBtn.setBackground(Color.BLACK);
+        stockInOutPanel.add(stockInBtn);
+
+        stockOutBtn = initButton(stockOutBtn, "Stock Out");
+        stockOutBtn.setBounds(stockInBtn.getX() + stockInBtn.getWidth() + 150,
+                                stockInBtn.getY() ,180, 50);
+        stockOutBtn.setForeground(Color.WHITE);
+        stockOutBtn.setBackground(Color.BLACK);
+        stockInOutPanel.add(stockOutBtn);
+    }
+    // ------------------------------------------------------------------------------
+
+    // initializers
+    // ------------------------------------------------------------------------------
+    private JLabel initLabel(JLabel label, String name){
+        label = new JLabel(name);
+        label.setBackground(Color.BLACK);
+        label.setFont(new Font("Rockwell", Font.BOLD, 25));
+        return label;
     }
 
     private JButton initButton(JButton button, String buttonName){
@@ -90,145 +255,13 @@ public class Stock extends JFrame implements ActionListener {
         return button;
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        if(e.getSource() == addBtn){
-            System.out.println("add btn");
-            highlightBtn(addBtn);
-            showPanelStack("Add Item Panel");
-        }
-        else if (e.getSource() == stockInBtn){
-            highlightBtn(stockInBtn);
-            System.out.println("stockIn btn");
-            showPanelStack("check");
-        }
-        else if (e.getSource() == stockOutBtn){
-            highlightBtn(stockOutBtn);
-            System.out.println("stockOut btn");
-        }
-        else if (e.getSource() == removeBtn){
-            highlightBtn(removeBtn);
-            System.out.println("remove btn");
-        }
-        else if (e.getSource() == updateBtn){
-            highlightBtn(updateBtn);
-            System.out.println("Update btn");
-        }
-    }
-
-    protected void setPanelStack(){
-
-        int x = buttonsPanel.getWidth() + 10;
-        int width = stockPanel.getWidth() - buttonsPanel.getWidth() - 10;
-
-        panelStack = new JPanel(new CardLayout());
-        panelStack.setBounds(x, 0, width, stockPanel.getHeight());
-
-        panelStack.add(addItemPanel(), "Add Item Panel");
-        stockPanel.add(panelStack);
-    }
-
-    // Displays the panel stored in workPanel's CardLayout wrt panel's name
-    private void showPanelStack(String panelName){
-        CardLayout cl = (CardLayout) (panelStack.getLayout());
-        cl.show(panelStack, panelName);
-    }
-
-    protected JPanel addItemPanel(){
-
-        addItemPanel = new JPanel();
-        // setBounds not needed as the panelStack is working in CardLayout
-        addItemPanel.setLayout(null);
-        addItemPanel.setBackground(Color.WHITE);
-
-        setAddItemLabels();
-        setAddItemTextField();
-
-        return addItemPanel;
-    }
-
-    private void setAddItemLabels() {
-
-        itemLabel = new JLabel("Item name");
-        itemLabel.setBounds(50, 55, 170, 30);
-        initLabel(itemLabel);
-
-        categoryLabel = new JLabel("Category");
-        categoryLabel.setBounds(itemLabel.getWidth() + 250, itemLabel.getY(), 150, 30);
-        initLabel(categoryLabel);
-
-        purchaseLabel = new JLabel("Purchase Price");
-        purchaseLabel.setBounds(itemLabel.getX(), itemLabel.getY() + 100, 200, 30);
-        initLabel(purchaseLabel);
-
-        sellingLabel = new JLabel("Selling Price");
-        sellingLabel.setBounds(itemLabel.getWidth() + 250, purchaseLabel.getY(), 200, 30);
-        initLabel(sellingLabel);
-
-        partyLabel = new JLabel("Party");
-        partyLabel.setBounds(itemLabel.getX(), purchaseLabel.getY() + 100, 80, 30);
-        initLabel(partyLabel);
-
-        priceUnitLabel = new JLabel("Price Unit");
-        priceUnitLabel.setBounds(itemLabel.getWidth() + 250, partyLabel.getY(), 200, 30);
-        initLabel(priceUnitLabel);
-
-        openingStockLabel = new JLabel("Opening Stock");
-        openingStockLabel.setBounds(itemLabel.getX(), partyLabel.getY() + 100, 200, 30);
-        initLabel(openingStockLabel);
-    }
-
-    private void initLabel(JLabel label){
-        label.setBackground(Color.BLACK);
-        label.setFont(new Font("Rockwell", Font.BOLD, 25));
-        addItemPanel.add(label);
-    }
-
-    private void setAddItemTextField() {
-
-        itemTF = new JTextField();
-        itemTF.setBounds(itemLabel.getX(), itemLabel.getY() + itemLabel.getHeight() + 7, 200, 30);
-        initTextField(itemTF);
-
-        categoryTF = new JTextField();
-        categoryTF.setBounds(categoryLabel.getX(), categoryLabel.getY() + categoryLabel.getHeight() + 7, 140, 30);
-        initTextField(categoryTF);
-
-        purchaseTF = new JTextField();
-        purchaseTF.setBounds(purchaseLabel.getX(), purchaseLabel.getY() + purchaseLabel.getHeight() + 7, 120, 30);
-        initTextField(purchaseTF);
-
-        sellingTF = new JTextField();
-        sellingTF.setBounds(sellingLabel.getX(), sellingLabel.getY() + sellingLabel.getHeight() + 7, 120, 30);
-        initTextField(sellingTF);
-
-        partyTF = new JTextField();
-        partyTF.setBounds(partyLabel.getX(), partyLabel.getY() + partyLabel.getHeight() + 7, 150, 30);
-        initTextField(partyTF);
-
-        // for price unit (combo box)
-        String[] elements = {"Hello", "Hi"};
-        priceUnit = new JComboBox<>(elements);
-        priceUnit.setBounds(priceUnitLabel.getX(), priceUnitLabel.getY() + priceUnitLabel.getHeight() + 7, 120, 30);
-        addItemPanel.add(priceUnit);
-
-        openingStockTF = new JTextField();
-        openingStockTF.setBounds(openingStockLabel.getX(), openingStockLabel.getY() + openingStockLabel.getHeight() + 7, 120, 30);
-        initTextField(openingStockTF);
-
-        doneBtn = initButton(doneBtn, "Done");
-        doneBtn.setBounds(categoryLabel.getX() - 150,openingStockTF.getY() + openingStockTF.getHeight() + 100, 150, 50);
-        doneBtn.setForeground(Color.WHITE);
-        doneBtn.setBackground(Color.BLACK);
-        addItemPanel.add(doneBtn);
-    }
-
-    private void initTextField(JTextField TxtF){
+    private JTextField initTextField(JTextField TxtF){
+        TxtF = new JTextField();
         TxtF.setMargin(new Insets(0, 4, 0, 4));
         TxtF.setFont(new Font("Rockwell", Font.PLAIN, 17));
-        addItemPanel.add(TxtF);
+        return TxtF;
     }
+    // ------------------------------------------------------------------------------
 
     private void highlightBtn(JButton currentBtn){
 
@@ -236,5 +269,42 @@ public class Stock extends JFrame implements ActionListener {
         currentBtn.setForeground(Color.BLACK);
 
         prevBtn = currentBtn;
+    }
+
+    protected JPanel set_getAddItemPanel(){
+        addItemPanel = new JPanel();
+        // setBounds not needed as the panelStack is working in CardLayout
+        addItemPanel.setLayout(null);
+        addItemPanel.setBackground(Color.WHITE);
+        setAddItemLabels();
+        setAddItemTextField();
+        return addItemPanel;
+    }
+
+    private JPanel set_getStockInOutPanel(){
+        stockInOutPanel = new JPanel();
+        stockInOutPanel.setLayout(null);
+        stockInOutPanel.setBackground(Color.WHITE);
+        setStockInOutLabels();
+        setStockInOutTextF_ComboBox();
+        return stockInOutPanel;
+    }
+
+    protected void setPanelStack(){
+        int x = buttonsPanel.getWidth() + 10;
+        int width = stockPanel.getWidth() - buttonsPanel.getWidth() - 10;
+
+        panelStack = new JPanel(new CardLayout());
+        panelStack.setBounds(x, 0, width, stockPanel.getHeight());
+
+        panelStack.add(set_getAddItemPanel(), "Add Item Panel");
+        panelStack.add(set_getStockInOutPanel(), "Stock In Out Panel");
+        stockPanel.add(panelStack);
+    }
+
+    // Displays the panel stored in workPanel's CardLayout wrt panel's name
+    private void showPanelStack(String panelName){
+        CardLayout cl = (CardLayout) (panelStack.getLayout());
+        cl.show(panelStack, panelName);
     }
 }
